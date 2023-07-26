8ed8f1de0d70f0dffec0e092f4d3e847d792e91a
Yastrzhemska <yastrzhemska@gmail.com>
2023-07-26 19:35:19 +0300

    update

---
+++ src/cat-api.js
@@ -0,0 +1,56 @@
+
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const END_POINT = '/breeds';
+const API_KEY = "live_ONDE7whOOmjntb5OU3N9yt0DEKUHLmooJCG0SMNWsnyLgUda590ArBWLJ3b4xaQo";
+
+export function fetchBreeds() {
+    return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        // .then(data => console.log(data))
+        // .catch(err => console.error(err))
+    
+};
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        });  
+};
+
+
+
+
+
+// import axios from 'axios';
+// axios.defaults.headers.common['x-api-key'] =
+// 'live_H24JXYBVrrM9Zqd0AhOom07voavPNPg9MNCpoSpk17G91i6mXMnj02i3p6IfEKdP';
+// axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+
+// export function fetchBreeds(endPoint) {
+// return axios.get(endPoint).then(data => {
+//     return data.data;
+// });
+// }
+
+// export function fetchCatByBreed(endPoint, breedId) {
+// const catByBreed = endPoint + '?breed_ids=' + breedId;
+// return axios.get(catByBreed).then(resp => {
+//     return resp.data;
+// });
+// }
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,82 @@
 
 
-
+import { fetchBreeds, fetchCatByBreed } from './cat-api'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
+import './styles.css';
 
 const selectors = {
-    select: document.querySelector('breed-select')
+    select: document.querySelector('.breed-select'),
+    load: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+    info: document.querySelector('.cat-info')
 }
+// console.log(selectors);
 
-console.log(selectors);
+selectors.load.classList.replace('loader', 'is-hidden');
+selectors.error.classList.add('is-hidden');
+selectors.info.classList.add('is-hidden');
+
+fetchBreeds().then(data => {
+    console.log(data)
+    
+    selectors.select.innerHTML = data.map(element => `<option value="${element.id}">${element.name}</option>`).join('');
+
+    new SlimSelect({
+        select: selectors.select,
+        
+    })
+    })
+    .catch(onFetchError)
+    .finally(() => selectors.load.setAttribute(`hidden`, true),
+        selectors.select.removeAttribute('hidden'));
+
+selectors.select.addEventListener('change', hendlerChangeSelect)
+
+function hendlerChangeSelect(evt) {
+    evt.preventDefault();
+    selectors.load.classList.replace('is-hidden', 'loader');
+    selectors.select.classList.add('is-hidden');
+    selectors.info.classList.add('is-hidden');
+
+    const breedId = evt.currentTarget.value;
+    fetchCatByBreed(breedId)
+        .then(data => {
+            console.log(data)
+        selectors.load.classList.replace('loader', 'is-hidden');
+        selectors.select.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+        
+            selectors.info.innerHTML = `
+        <img src="${url}" 
+        alt="${breeds[0].name}" 
+        width="400"/>
+
+        <div class="contenet">
+        <h2>${breeds[0].name}</h2>
+        <p>${breeds[0].description}</p>
+        <p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
+        selectors.info.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+};
+
+
+function onFetchError(err) {
+    selectors.select.classList.remove('is-hidden');
+    selectors.load.classList.replace('loader', 'is-hidden');
+
+    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+        position: 'center-center',
+        timeout: 3000,
+        width: '500px',
+        fontSize: '30px'
+    });
+};
+
+
+
+
+
+
