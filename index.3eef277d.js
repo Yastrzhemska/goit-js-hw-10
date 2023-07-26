c86856a2595563267c579c036520b85a56287072
Yastrzhemska <125347116+Yastrzhemska@users.noreply.github.com>
2023-07-26 16:35:48 +0000

    Deploying to gh-pages from @ Yastrzhemska/goit-js-hw-10@8ed8f1de0d70f0dffec0e092f4d3e847d792e91a 🚀

--- index.3eef277d.js
+++
@@ -1,2 +0,0 @@
-const e={select:document.querySelector("breed-select")};console.log(e);
-//# sourceMappingURL=index.3eef277d.js.map
--- index.e5fc8228.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={select:document.querySelector("breed-select")};console.log(e)}();
-//# sourceMappingURL=index.e5fc8228.js.map
